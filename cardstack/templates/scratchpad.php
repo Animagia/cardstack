b5e331d8ef676dfe901276c5594bacb8ad03d484
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2018-05-04 21:18:31 +0200

    Add basic subscription checking prototype

--- cardstack/templates/scratchpad.php
+++ cardstack/templates/scratchpad.php
@@ -7,6 +7,15 @@
 
 <?php get_header(); ?>
 
+<?php
+
+function isActive($sub) {
+    $subdata = $sub->data;
+    
+}
+
+?>
+
 <main<?php
 if (is_active_sidebar(1)) {
     print(' class="with-sidebar"');
@@ -22,15 +31,27 @@ if (is_active_sidebar(1)) {
 
                 <h1><?php the_title(); ?></h1>
 
-                <p>Custom stuff goes in this paragraph. Did it sync? RSS_1337</p>
+                <p>Custom paragraph.</p>
 
-                <?php echo('userid' . get_current_user_id()); ?>
+				<?php $subscriptions = hforce_get_users_subscriptions();
+				
+                                foreach($subscriptions as $subscription) {
+                                    
+                                }
+                                
+				$subdata = $subscriptions[113]->data;
+				
+				$substatus = $subdata->status;
+				
+				$scheduleEndString = $subdata->schedule_end;
+				
+				echo ("<p>Stuff: " . $subdata["status"] . " " . $subdata["schedule_end"] . "</p>");
+				
+				 ?>
+				
+                <pre><?php var_dump($subscriptions[113]->data); ?></pre>
 
-                <p><?php var_dump(hforce_get_users_subscriptions()); ?></p>
-
-                <p><?php var_dump(hforce_get_users_subscriptions(get_current_user_id())); ?></p>
-
-                <p><?php echo(hforce_dummy_nonexistent_functions()); ?></p>
+                <pre><?php var_dump(hforce_get_users_subscriptions()); ?></pre>
 
                 <?php the_content(); ?> 
 
