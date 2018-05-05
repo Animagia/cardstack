ccb67b95fdb22644e53e62146c4c3037e2e10627
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2018-05-05 13:35:51 +0200

    Establish all-in-one account management page

--- cardstack/templates/scratchpad.php
+++ cardstack/templates/scratchpad.php
@@ -22,22 +22,42 @@ if (is_active_sidebar(1)) {
 
                 <h1><?php the_title(); ?></h1>
 
-                <p>Custom paragraph.</p>
-
                 <?php
-                $substatus = $cardstack_am->getSubStatus();
+                if (!is_user_logged_in()) :
 
-                echo ("<p>Status: " . $substatus . "</p>");
+                    wp_login_form();
 
-                $date = new DateTime("now", new DateTimeZone("Europe/Warsaw"));
-                $date->setTimestamp($cardstack_am->getExpirationDate());
+                else :
+                    ?>
 
-                if ($substatus == "expiring") {
-                    echo ("<p>Valid until: " . $date->format("Y-m-d H:i:s") . "</p>");
-                }
-                ?>
+                    <p>Custom paragraph.</p>
 
-                <?php the_content(); ?> 
+
+                    <?php
+                    $substatus = $cardstack_am->getSubStatus();
+
+                    echo ("<p>Status: " . $substatus . "</p>");
+
+                    $date = new DateTime("now", new DateTimeZone("Europe/Warsaw"));
+                    $date->setTimestamp($cardstack_am->getExpirationDate());
+
+                    if ($substatus == "expiring") {
+                        echo ("<p>Valid until: " . $date->format("Y-m-d H:i:s") . "</p>");
+                    }
+
+
+                    //amagi id: 118
+
+                    $current_user = wp_get_current_user();
+
+                    if (wc_customer_bought_product($current_user->user_email, $current_user->ID, 118)) {
+                        echo ("This user bought Amagi!");
+                    }
+                    ?>
+
+                    <?php the_content(); ?> 
+
+                <?php endif; ?>
 
                 <?php comments_template(); ?>
 
