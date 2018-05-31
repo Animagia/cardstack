b44c6e020092561d0360d170ac97aad21278d96e
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2018-05-31 18:12:31 +0200

    Fix duplicate line

--- cardstack/header.php
+++ cardstack/header.php
@@ -30,7 +30,6 @@
           var _paq = _paq || [];
           /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
           _paq.push(['disableCookies']);
-          _paq.push(['trackPageView']);
           
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
