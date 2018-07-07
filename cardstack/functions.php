52f929dc9d60e0cdd587c088be0541fab4efd763
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2018-07-07 08:00:54 +0200

    Limit WC scripts to pages where they are needed

--- cardstack/functions.php
+++ cardstack/functions.php
@@ -667,3 +667,40 @@ add_filter( 'woocommerce_available_payment_gateways', 'cardstack_am_disable_coun
 remove_action('woocommerce_review_order_after_order_total', array(HForce_Subscription_Cart, 'display_recurring_totals'));
 remove_action('woocommerce_cart_totals_after_order_total', array(HForce_Subscription_Cart, 'display_recurring_totals'));
 
+
+/* Only load WC scripts and styles on cart/checkout page */
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_styles_scripts', 99 );
+
+function dequeue_woocommerce_styles_scripts() {
+    if ( function_exists( 'is_woocommerce' ) ) {
+        if ( /* ! is_woocommerce()  && */ ! is_cart() && ! is_checkout() ) {
+            # Styles
+            wp_dequeue_style( 'woocommerce-general' );
+            wp_dequeue_style( 'woocommerce-layout' );
+            wp_dequeue_style( 'woocommerce-smallscreen' );
+            wp_dequeue_style( 'woocommerce_frontend_styles' );
+            wp_dequeue_style( 'woocommerce_fancybox_styles' );
+            wp_dequeue_style( 'woocommerce_chosen_styles' );
+            wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+            # Scripts
+            wp_dequeue_script( 'wc_price_slider' );
+            wp_dequeue_script( 'wc-single-product' );
+            wp_dequeue_script( 'wc-add-to-cart' );
+            wp_dequeue_script( 'wc-cart-fragments' );
+            wp_dequeue_script( 'wc-checkout' );
+            wp_dequeue_script( 'wc-add-to-cart-variation' );
+            wp_dequeue_script( 'wc-single-product' );
+            wp_dequeue_script( 'wc-cart' );
+            wp_dequeue_script( 'wc-chosen' );
+            wp_dequeue_script( 'woocommerce' );
+            wp_dequeue_script( 'prettyPhoto' );
+            wp_dequeue_script( 'prettyPhoto-init' );
+            wp_dequeue_script( 'jquery-blockui' );
+            wp_dequeue_script( 'jquery-placeholder' );
+            wp_dequeue_script( 'fancybox' );
+            wp_dequeue_script( 'jqueryui' );
+        }
+    }
+}
+
+
