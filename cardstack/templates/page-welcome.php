958ac09bcad834c09a4fbcc1c678d883882a9699
Łukasz Klekowski <34490533+lukaszklekowski@users.noreply.github.com>
2018-09-05 17:01:57 +0200

    Make welcome page editable

--- cardstack/templates/page-welcome.php
+++ cardstack/templates/page-welcome.php
@@ -4,6 +4,7 @@
  */
 
 require_once( __DIR__ . '/../includes/video-player.php' );
+require_once( __DIR__ . '/../sample-content/page-welcome-content.php');
 ?>
 
 <?php get_header(); ?>
@@ -72,39 +73,16 @@ if (is_active_sidebar(1)) {
 
     </article>
 
-    <article class="page<?php
-    if (get_theme_mod('page_breadcrumbs')) {
-        print(' has-breadcrumbs');
-    }
-    ?>">
-        <figure class="container">
-            <img src="https://static.animagia.pl/Amagi1.jpg" alt="Amagi"/>
-            <div class="image-text">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper dapibus eros.
-                    Quisque et rutrum purus, semper rhoncus purus. Donec libero odio, eleifend a purus
-                    lobortis, accumsan cursus urna. Pellentesque et pharetra metus. Mauris lobortis
-                    metus eu tortor blandit, in viverra est ultricies.
-                </p>
-            </div>
-        </figure>
-    </article>
-    <aside class="aside">
-        <ul class="icon-container">
-            <li class="mmenuitem">
-                <img class="image" src="https://dev.animagia.pl/movie_creation.png" alt="movie"/>
-                <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li class="mmenuitem">
-                <img class="image" src="https://dev.animagia.pl/lock.png" alt="lock"/>
-                <p>Lorem ipsum dolor sit amet</p>
-            </li>
-            <li class="mmenuitem">
-                <img class="image" src="https://dev.animagia.pl/ondemand_video.png" alt="video"/>
-                <p>Lorem ipsum dolor sit amet</p>
-            </li>
-        </ul>
-    </aside>
+    <?php
+    $thecontent = get_the_content();
+    if(!empty($thecontent)) {
+
+        the_content();
+
+    } else {
+        CsAmWelcomeContent::printBanner();
+    } ?>
+
 </main>
 
 <?php get_footer(); ?>
