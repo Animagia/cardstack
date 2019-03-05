b409e66beb49b4224dab1621cfcbeab72ca7be97
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2019-03-05 17:38:30 +0100

    Redirect archives (/author/admin/, etc) to homepage

--- cardstack/404.php
+++ cardstack/404.php
@@ -2,4 +2,3 @@
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: ".get_bloginfo('url'));
 exit();
-?>
---
+++ cardstack/archive.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: ".get_bloginfo('url'));
+exit();
