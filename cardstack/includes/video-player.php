cfe8c716b67fcab52ce87b9accb660d772696c65
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2019-04-13 18:34:22 +0200

    Clean up video player

--- cardstack/includes/video-player.php
+++ cardstack/includes/video-player.php
@@ -26,6 +26,8 @@ class CsAmVideo {
             $csam_preview_length = 772;
         } else if ("Future" == $csam_short_name) {
             $csam_preview_length = 768;
+        } else if ("Tama" == $csam_short_name) {
+            $csam_preview_length = 645;
         }
 
 ?>
@@ -35,7 +37,7 @@ class CsAmVideo {
             <a href="<?php echo get_home_url() ?>/sklep/">kont premium</a>.</p>
 
 
-        <video onerror="loadError();" id='amagi' class="video-js vjs-16-9 vjs-big-play-centered" style="width: 100%;"
+        <video onerror="onLoadError();" id='amagi' class="video-js vjs-16-9 vjs-big-play-centered" style="width: 100%;"
                controls="true" oncontextmenu="return false;"
                poster="<?php echo $csam_poster ?>" preload="metadata"
                data-setup='{}'>
@@ -46,50 +48,39 @@ class CsAmVideo {
 
         <script>
             var player = videojs('amagi');
-            var isPlaying = false;
-	function makeRequest(){
-		var elem = document.getElementById("amagi");
+            
+            function makeRequest(){
     		var xhr = new XMLHttpRequest();
-   		var link="<?php echo home_url() ?>";
-    //const fd = new FormData();
-    //fd.append('videofile', document.getElementById("amagi").files[0]);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-           console.log(xhr.responseText);    }
- 		var positionStart=xhr.responseText.search("token=")+6;
-      		var positionEnd=xhr.responseText.search('type="video/webm"')-2;
-        	var token=xhr.responseText.substring(positionStart,positionEnd);
-		console.log(token);
-	var blob_uri = URL.createObjectURL(xhr.response);
- 	elem.appendChild(document.createElement("source"))
-                    .src = blob_uri;
-    }
-    	xhr.open('POST',link,true);
-    	xhr.send(null);
-    }
-    	    function loadError() {
-
-            console.warn("Playback has not started - expired token?");
-makeRequest();
+   		var linkToCurrentPage="<?php echo get_permalink() ?>";
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        var positionStart=xhr.responseText.search("token=")+6;
+                        var positionEnd=xhr.responseText.search('type="video/webm"')-2;
+                        var token=xhr.responseText.substring(positionStart,positionEnd);
+                        console.log("New token: " + token);
+                    }
+                }
+                xhr.open('POST',linkToCurrentPage,true);
+                xhr.send(null);
             }
+            
+    	    function onLoadError() {
+                console.warn("Playback has not started - expired token?");
+                makeRequest();
+            }
+            
             player.on('waiting', function() {
-            isPlaying = false;
-makeRequest();
-	    setTimeout(function(){ loadError(); }, 4000);
-            });
-
-            player.on('playing', function() {
-            isPlaying = true;
+                makeRequest();
+                setTimeout(function(){ onLoadError(); }, 4000);
             });
 
             player.on('dblclick', function () {
                 player.requestFullscreen();
             });
+            
             player.on('dblclick', function () {
                 player.exitFullscreen();
             });
-            
-            var modal = null;
 
             player.on('timeupdate', function () {
                 var vid1time = player.currentTime();
@@ -98,8 +89,6 @@ makeRequest();
                     player.pause();
                     player.currentTime(<?php print(($csam_preview_length-5)); ?>);
                 }
-        
-
             });
 		
             
