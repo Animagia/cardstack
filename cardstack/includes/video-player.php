8ce07e615c648bb0626f8aef2c624e8a1a253bcc
pjanocha <49397278+pjanocha@users.noreply.github.com>
2019-04-19 21:05:06 +0200

    Update video-player.php

--- cardstack/includes/video-player.php
+++ cardstack/includes/video-player.php
@@ -9,9 +9,14 @@ class CsAmVideo {
         $cardstack_am_pure_stream_str = $csam_short_name . "_2_" . time() .
                 "_" . $_SERVER['REMOTE_ADDR'];
         $cardstack_am_stream_token = CardStackAm::obfuscateString($cardstack_am_pure_stream_str);
+	$csam_append_token_to_this = CardStackAmConstants::getVidUrl() .
+                "stream/film_stream.php/" . $csam_short_name . $cardstack_am_episode . ".webm?token=";
         $cardstack_am_video = CardStackAmConstants::getVidUrl() .
                 "stream/film_stream.php/" . $csam_short_name . $cardstack_am_episode . ".webm?token=" .
-                $cardstack_am_stream_token;
+                $cardstack_am_stream_token . "&t=" . time();
+	$cardstack_am_video2 = CardStackAmConstants::getVidUrl() .
+            "stream/film_stream.php/" . $csam_short_name . $cardstack_am_episode . "s" . ".webm?token=" .
+     	$cardstack_am_stream_token . "&t=" . time() ;
 
 
 
@@ -26,6 +31,8 @@ class CsAmVideo {
             $csam_preview_length = 772;
         } else if ("Future" == $csam_short_name) {
             $csam_preview_length = 768;
+        } else if ("Tama" == $csam_short_name) {
+            $csam_preview_length = 645;
         }
 
 ?>
@@ -35,7 +42,7 @@ class CsAmVideo {
             <a href="<?php echo get_home_url() ?>/sklep/">kont premium</a>.</p>
 
 
-        <video onerror="loadError();" id='amagi' class="video-js vjs-16-9 vjs-big-play-centered" style="width: 100%;"
+        <video onerror="onLoadError();" id='amagi' class="video-js vjs-16-9 vjs-big-play-centered" style="width: 100%;"
                controls="true" oncontextmenu="return false;"
                poster="<?php echo $csam_poster ?>" preload="metadata"
                data-setup='{}'>
@@ -43,53 +50,85 @@ class CsAmVideo {
         </video>
 
         <script src="https://static.animagia.pl/video.js"></script>
-
-        <script>
+	<script src="https://animagia.pl/resSwitcher.js"></script>
+        
+<script>
+//test	
+   videojs('amagi', {
+    controls: true,
+      muted: false,
+      width: 1000,
+    plugins: {
+      videoJsResolutionSwitcher: {
+        default: 'SD', // Default resolution [{Number}, 'low', 'high'],
+        dynamicLabel: true
+      }
+    }
+  }, function(){
+    var player = this;
+    window.player = player
+    player.updateSrc([
+      {
+        src: '<?php echo $cardstack_am_video ?>',
+        type: 'video/webm',
+        label: 'SD',
+        res: 360
+      },
+      {
+        src: '<?php echo $cardstack_am_video2 ?>',
+        type: 'video/webm',
+        label: 'HD',
+        res: 720
+      }
+    ])
+	
+    player.on('resolutionchange', function(){
+      console.info('Source changed to %s', player.src())
+    })
+  })
             var player = videojs('amagi');
-            var isPlaying = false;
-	function makeRequest(){
-		var elem = document.getElementById("amagi");
+		
+            function makeRequest(){
+		var source = document.createElement('source');
     		var xhr = new XMLHttpRequest();
-   		var link="<?php echo home_url() ?>";
-    //const fd = new FormData();
-    //fd.append('videofile', document.getElementById("amagi").files[0]);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-           console.log(xhr.responseText);    }
- 		var positionStart=xhr.responseText.search("token=")+6;
-      		var positionEnd=xhr.responseText.search('type="video/webm"')-2;
-        	var token=xhr.responseText.substring(positionStart,positionEnd);
-		console.log(token);
-	var blob_uri = URL.createObjectURL(xhr.response);
- 	elem.appendChild(document.createElement("source"))
-                    .src = blob_uri;
-    }
-    	xhr.open('POST',link,true);
-    	xhr.send(null);
-    }
-    	    function loadError() {
-
-            console.warn("Playback has not started - expired token?");
-makeRequest();
+   		var linkToCurrentPage="<?php echo get_permalink() ?>";
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        var positionStart=xhr.responseText.search("token=")+6;
+                        var positionEnd=xhr.responseText.search('type="video/webm"')-2;
+                        var token=xhr.responseText.substring(positionStart,positionEnd);
+			var newSrc = "<?php echo $csam_append_token_to_this ?>" + token;
+			source.setAttribute('src', newSrc);
+			source.setAttribute('type', 'video/webm');
+                        console.log("New token: " + token);
+			document.getElementById("amagi_html5_api").innerHTML = '';
+			
+			document.getElementById("amagi_html5_api").appendChild(source);  
+             		//player.reset();
+                       // player.src({type: 'video/webm', src: newSrc, withCredentials: false});
+ }
+                }
+                xhr.open('POST',linkToCurrentPage,true);
+                xhr.send(null);
             }
+            
+    	    function onLoadError() {
+                console.warn("Playback has not started - expired token?");
+                makeRequest();
+            }
+            
             player.on('waiting', function() {
-            isPlaying = false;
-makeRequest();
-	    setTimeout(function(){ loadError(); }, 4000);
-            });
-
-            player.on('playing', function() {
-            isPlaying = true;
+                makeRequest();
+                setTimeout(function(){ onLoadError(); }, 4000);
             });
 
             player.on('dblclick', function () {
                 player.requestFullscreen();
             });
+            
             player.on('dblclick', function () {
                 player.exitFullscreen();
             });
-            
-            var modal = null;
 
             player.on('timeupdate', function () {
                 var vid1time = player.currentTime();
@@ -98,8 +137,6 @@ makeRequest();
                     player.pause();
                     player.currentTime(<?php print(($csam_preview_length-5)); ?>);
                 }
-        
-
             });
 		
             
@@ -120,8 +157,12 @@ makeRequest();
         }
         $cardstack_am_video = CardStackAmConstants::getVidUrl() .
             "stream/film_stream.php/" . $csam_short_name . $cardstack_am_episode . ".webm?token=" .
-            $cardstack_am_stream_token;
+     	$cardstack_am_stream_token . "&t=" . time() ;
         $cardstack_am_poster = "https://static.animagia.pl/" . $csam_short_name . "_poster.jpg";
+
+	$cardstack_am_video2 = CardStackAmConstants::getVidUrl() .
+            "stream/film_stream.php/" . $csam_short_name . $cardstack_am_episode . "s" . ".webm?token=" .
+     	$cardstack_am_stream_token . "&t=" . time() ;
 
         if (IP_Geo_Block::get_geolocation()['code'] !== 'PL') {
             $cardstack_am_video = "";
@@ -142,10 +183,46 @@ makeRequest();
                data-setup='{}'>
             <source src="<?php echo $cardstack_am_video ?>" type="video/webm" />
         </video>
-
         <script src="https://static.animagia.pl/video.js"></script>
+	<script src="https://animagia.pl/resSwitcher.js"></script>
 
         <script>
+//premium
+	videojs('amagi', {
+    controls: true,
+      muted: false,
+      width: 1000,
+    plugins: {
+      videoJsResolutionSwitcher: {
+        default: 'SD', // Default resolution [{Number}, 'low', 'high'],
+        dynamicLabel: true
+      }
+    }
+  }, function(){
+    var player = this;
+    window.player = player
+    player.updateSrc([
+      {
+	
+        src: '<?php echo $cardstack_am_video ?>',
+        type: 'video/webm',
+        label: 'SD',
+        res: 360
+      },
+      {
+        src: '<?php echo $cardstack_am_video2 ?>',
+        type: 'video/webm',
+        label: 'HD',
+        res: 720
+      }
+    ])
+	
+    player.on('resolutionchange', function(){
+      console.info('Source changed to %s', player.src())
+    })
+  })
+	
+	
             var vid1 = videojs('amagi');
             vid1.on('dblclick', function () {
                 vid1.requestFullscreen();
